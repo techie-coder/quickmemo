db459131b1bffb6a5698088be3e15cd5ebb7de1b
Saswata Biswas <saswata.biswas20@gmail.com>
2025-01-18 20:40:30 +0530

    Add files via upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,1 +1,15 @@
+import LandingPage from './components/LandingPage'
+import NavBar from './components/NavBar';
 
+const QuickMemo = () => {
+  return(
+    <>
+    <div className='h-full bg-black'>
+    <NavBar/>
+    <LandingPage/>
+    </div>
+    </>
+  )
+}
+
+export default QuickMemo;
